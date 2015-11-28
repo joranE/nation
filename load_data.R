bc1cbc486ec95b7dc5284a33a448b76f29712db6
joran <joran.elias@gmail.com>
2015-11-27 18:29:03 -0700

    switch to remote data

--- load_data.R
+++ load_data.R
@@ -2,5 +2,10 @@
 # DATA <- read_csv("fis.csv",
 # 								col_names = TRUE,
 # 								col_types = "iiccccdccccccciiciidd")
-FIS <- src_sqlite("fis.sqlite3",create = FALSE)
+#FIS <- src_sqlite("fis.sqlite3",create = FALSE)
+FIS <- src_mysql(options()$mysql$dbName,
+                 options()$mysql$host,
+                 options()$mysql$port,
+                 options()$mysql$user,
+                 options()$mysql$password)
 DATA <- tbl(FIS,"main")
