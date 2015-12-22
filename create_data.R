76e2d46f9c6ef2f93421c9dc26bbb61ee734c623
Joran Elias <joran.elias@umontana.edu>
2015-12-22 16:13:10 -0700

    update create_data.R

--- create_data.R
+++ create_data.R
@@ -1,11 +1,11 @@
-library(statskier)
+library(statskier2)
 library(dplyr)
-con <- db_xc()
-x <- statskier::query(con,"select * from main")
+conl <- db_xc_local()
+x <- statskier2::ss_query(conl,"select * from main")
 
 x$cat2[x$cat2 == ""] <- NA
 
-out <- filter(x,cat1 %in% c('WC','WSC','OWG','TDS','WJC','U23'))
+out <- dplyr::filter(x,cat1 %in% c('WC','WSC','OWG','TDS','WJC','U23'))
 # 
 # write.table(x = out,
 #             file = "fis.csv",
